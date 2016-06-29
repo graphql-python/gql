818f8e68f2cd1dce9fc411fd7a6b2b3a0bdd1798
Eran Kampf <eran@ekampf.com>
2016-06-29 15:48:15 -0700

    Added retry logic to Client
    retry on transport errors

--- gql/client.py
+++ gql/client.py
@@ -1,13 +1,22 @@
+import logging
+
 from graphql import parse, introspection_query, build_ast_schema, build_client_schema
 from graphql.validation import validate
 
 from .transport.local_schema import LocalSchemaTransport
 
 
-class Client(object):
+class RetryError(Exception):
+    """Custom exception thrown when retry logic fails"""
+    def __init__(self, retries_count, last_exception):
+        message = "Failed %s retries: %s" % (retries_count, last_exception)
+        super(RetryError, self).__init__(message)
+        self.last_exception = last_exception
 
+
+class Client(object):
     def __init__(self, schema=None, introspection=None, type_def=None, transport=None,
-                 fetch_schema_from_transport=False):
+                 fetch_schema_from_transport=False, retries=0):
         assert not(type_def and introspection), 'Cant provide introspection type definition at the same time'
         if transport and fetch_schema_from_transport:
             assert not schema, 'Cant fetch the schema from transport if is already provided'
@@ -25,6 +34,7 @@ class Client(object):
         self.schema = schema
         self.introspection = introspection
         self.transport = transport
+        self.retries = retries
 
     def validate(self, document):
         if not self.schema:
@@ -36,7 +46,29 @@ class Client(object):
     def execute(self, document, *args, **kwargs):
         if self.schema:
             self.validate(document)
-        result = self.transport.execute(document, *args, **kwargs)
+
+        result = self._get_result(document, *args, **kwargs)
         if result.errors:
             raise result.errors[0]
+
         return result.data
+
+    def _get_result(self, document, *args, **kwargs):
+        if not self.retries:
+            return self.transport.execute(document, *args, **kwargs)
+
+        last_exception = None
+        retries_count = 0
+        while retries_count < self.retries:
+            try:
+                result = self.transport.execute(document, *args, **kwargs)
+                return result
+            except Exception as e:
+                last_exception = e
+                logging.warn("Request failed with exception %s. Retrying for the %s time...", retries_count + 1, exc_info=True)
+            finally:
+                retries_count += 1
+
+        raise RetryError(retries_count, last_exception)
+
+
