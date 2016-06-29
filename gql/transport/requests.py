6d64cf77cb621f5c5c58e8b81313e04b21c9c3bb
Eran Kampf <eran@ekampf.com>
2016-06-29 14:47:11 -0700

    Support requests auth parameter

--- gql/transport/requests.py
+++ gql/transport/requests.py
@@ -8,6 +8,9 @@ from .http import HTTPTransport
 
 
 class RequestsHTTPTransport(HTTPTransport):
+    def __init__(self, auth=None, *args, **kwargs):
+        super(RequestsHTTPTransport, self).__init__(*args, **kwargs)
+        self.auth = auth
 
     def execute(self, document, variable_values=None):
         query_str = print_ast(document)
@@ -17,7 +20,8 @@ class RequestsHTTPTransport(HTTPTransport):
                 'query': query_str,
                 'variables': variable_values
             },
-            headers=self.client_headers
+            headers=self.client_headers,
+            auth=self.auth
         )
         result = request.json()
         assert 'errors' in result or 'data' in result, 'Received non-compatible response "{}"'.format(result)
