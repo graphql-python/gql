03f3f8d95e9f04e724584c01dbbb08280877d627
KingDarBoja <stevenbojato04@gmail.com>
2020-02-23 20:52:14 -0500

    test: no schema and execute client error

--- tests/test_client.py
+++ tests/test_client.py
@@ -29,3 +29,38 @@ def test_retries(execute_mock):
         client.execute(query)
 
     assert execute_mock.call_count == expected_retries
+
+
+def test_no_schema_exception():
+    with pytest.raises(Exception) as excInfo:
+        client = Client()
+        client.validate('')
+    assert "Cannot validate locally the document, you need to pass a schema." in str(excInfo.value)
+
+
+def test_execute_result_error():
+    expected_retries = 3
+
+    client = Client(
+        retries=expected_retries,
+        transport=RequestsHTTPTransport(
+            url='https://countries.trevorblades.com/',
+            use_json=True,
+            headers={
+                "Content-type": "application/json",
+            }
+        )
+    )
+
+    query = gql('''
+    query getContinents {
+      continents {
+        code
+        name
+        id
+      }
+    }
+    ''')
+    with pytest.raises(Exception) as excInfo:
+        client.execute(query)
+    assert "Cannot query field \"id\" on type \"Continent\"." in str(excInfo.value)
