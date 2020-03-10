a0fdd4c83cb9d366bad571df32248911988a98e9
KingDarBoja <stevenbojato04@gmail.com>
2020-03-09 22:53:59 -0500

    refactor: ABC Transport for py27

--- gql/transport/transport.py
+++ gql/transport/transport.py
@@ -1,14 +1,15 @@
-from abc import ABC, abstractmethod
+import abc
 from typing import Union
 
+import six
 from graphql.execution import ExecutionResult
 from graphql.language.ast import Node, Document
 from promise import Promise
 
 
-class Transport(ABC):
-
-    @abstractmethod
+@six.add_metaclass(abc.ABCMeta)
+class Transport:
+    @abc.abstractmethod
     def execute(self, document):
         # type: (Union[Node, Document]) -> Union[ExecutionResult, Promise[ExecutionResult]]
         """Execute the provided document AST for either a remote or local GraphQL Schema.
