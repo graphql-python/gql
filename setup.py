fce1de36f48f31fefc9596ae3d13c61e935664ab
Christoph Zwerschke <cito@online.de>
2020-03-11 22:51:31 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ dev_requires = [
 
 setup(
     name='gql',
-    version='0.3.0',
+    version='0.4.0',
     description='GraphQL client for Python',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
