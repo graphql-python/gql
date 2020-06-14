a21ff73b2e8838ecd2f1ba27090598d264ac58cd
Hanusz Leszek <leszek.hanusz@gmail.com>
2020-06-14 22:57:49 +0200

    Use less generic exceptions (#95)

--- gql/gql.py
+++ gql/gql.py
@@ -1,8 +1,6 @@
-from graphql import Source, parse
+from graphql import DocumentNode, Source, parse
 
 
-def gql(request_string):
-    if not isinstance(request_string, str):
-        raise Exception(f'Received incompatible request "{request_string}".')
+def gql(request_string: str) -> DocumentNode:
     source = Source(request_string, "GraphQL request")
     return parse(source)
