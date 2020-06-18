76fa98acacc0ebbcb2977cd56a62a1c2630f441b
Aurélien Catinon <aurelien.catinon@gmail.com>
2020-06-18 11:33:55 +0200

    Increase PhoenixChannelWebsocketsTransport unit tests coverage

--- tests/test_phoenix_channel_exceptions.py
+++ tests/test_phoenix_channel_exceptions.py
@@ -1,7 +1,11 @@
 import pytest
 
 from gql import Client, gql
-from gql.transport.exceptions import TransportProtocolError, TransportQueryError
+from gql.transport.exceptions import (
+    TransportProtocolError,
+    TransportQueryError,
+    TransportServerError,
+)
 from gql.transport.phoenix_channel_websockets import PhoenixChannelWebsocketsTransport
 
 from .conftest import PhoenixChannelServerHelper
@@ -15,8 +19,7 @@ query1_str = """
     }
 """
 
-
-subscription_server_answer = (
+default_subscription_server_answer = (
     '{"event":"phx_reply",'
     '"payload":'
     '{"response":'
@@ -25,7 +28,6 @@ subscription_server_answer = (
     '"ref":2,'
     '"topic":"test_topic"}'
 )
-
 
 error_server_answer = (
     '{"event":"phx_reply",'
@@ -37,16 +39,6 @@ error_server_answer = (
     '"topic":"test_topic"}'
 )
 
-
-async def phoenix_server_reply_error(ws, path):
-    await PhoenixChannelServerHelper.send_connection_ack(ws)
-    await ws.recv()
-    await ws.send(subscription_server_answer)
-    await ws.send(error_server_answer)
-    await PhoenixChannelServerHelper.send_close(ws)
-    await ws.wait_closed()
-
-
 timeout_server_answer = (
     '{"event":"phx_reply",'
     '"payload":'
@@ -56,48 +48,26 @@ timeout_server_answer = (
 )
 
 
-async def phoenix_server_timeout(ws, path):
-    await PhoenixChannelServerHelper.send_connection_ack(ws)
-    await ws.recv()
-    await ws.send(subscription_server_answer)
-    await ws.send(timeout_server_answer)
-    await PhoenixChannelServerHelper.send_close(ws)
-    await ws.wait_closed()
+def server(
+    query_server_answer, subscription_server_answer=default_subscription_server_answer,
+):
+    async def phoenix_server(ws, path):
+        await PhoenixChannelServerHelper.send_connection_ack(ws)
+        await ws.recv()
+        await ws.send(subscription_server_answer)
+        if query_server_answer is not None:
+            await ws.send(query_server_answer)
+        await PhoenixChannelServerHelper.send_close(ws)
+        await ws.wait_closed()
 
-
-generic_error_server_answer = (
-    '{"event":"phx_reply",'
-    '"payload":'
-    '{"status":"error"},'
-    '"ref":2,'
-    '"topic":"test_topic"}'
-)
-
-
-async def phoenix_server_generic_error(ws, path):
-    await PhoenixChannelServerHelper.send_connection_ack(ws)
-    await ws.recv()
-    await ws.send(subscription_server_answer)
-    await ws.send(generic_error_server_answer)
-    await PhoenixChannelServerHelper.send_close(ws)
-    await ws.wait_closed()
-
-
-protocol_server_answer = '{"event":"unknown"}'
-
-
-async def phoenix_server_protocol_error(ws, path):
-    await PhoenixChannelServerHelper.send_connection_ack(ws)
-    await ws.recv()
-    await ws.send(subscription_server_answer)
-    await ws.send(protocol_server_answer)
-    await PhoenixChannelServerHelper.send_close(ws)
-    await ws.wait_closed()
+    return phoenix_server
 
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
-    "server", [phoenix_server_reply_error, phoenix_server_timeout], indirect=True
+    "server",
+    [server(error_server_answer), server(timeout_server_answer)],
+    indirect=True,
 )
 @pytest.mark.parametrize("query_str", [query1_str])
 async def test_phoenix_channel_query_error(event_loop, server, query_str):
@@ -114,10 +84,65 @@ async def test_phoenix_channel_query_error(event_loop, server, query_str):
             await session.execute(query)
 
 
+invalid_subscription_id_server_answer = (
+    '{"event":"subscription:data","payload":'
+    '{"subscriptionId":"INVALID","result":'
+    '{"data":{"continents":['
+    '{"code":"AF","name":"Africa"},{"code":"AN","name":"Antarctica"},'
+    '{"code":"AS","name":"Asia"},{"code":"EU","name":"Europe"},'
+    '{"code":"NA","name":"North America"},{"code":"OC","name":"Oceania"},'
+    '{"code":"SA","name":"South America"}]}}},'
+    '"ref":3,'
+    '"topic":"test_topic"}'
+)
+
+invalid_payload_server_answer = (
+    '{"event":"subscription:data",'
+    '"payload":"INVALID",'
+    '"ref":3,'
+    '"topic":"test_topic"}'
+)
+
+invalid_result_server_answer = (
+    '{"event":"subscription:data","payload":'
+    '{"subscriptionId":"test_subscription","result": "INVALID"},'
+    '"ref":3,'
+    '"topic":"test_topic"}'
+)
+
+generic_error_server_answer = (
+    '{"event":"phx_reply",'
+    '"payload":'
+    '{"status":"error"},'
+    '"ref":2,'
+    '"topic":"test_topic"}'
+)
+
+protocol_server_answer = '{"event":"unknown"}'
+
+invalid_payload_subscription_server_answer = (
+    '{"event":"phx_reply", "payload":"INVALID", "ref":2, "topic":"test_topic"}'
+)
+
+
+async def no_connection_ack_phoenix_server(ws, path):
+    await ws.recv()
+    await PhoenixChannelServerHelper.send_close(ws)
+    await ws.wait_closed()
+
+
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     "server",
-    [phoenix_server_generic_error, phoenix_server_protocol_error],
+    [
+        server(invalid_subscription_id_server_answer),
+        server(invalid_result_server_answer),
+        server(generic_error_server_answer),
+        no_connection_ack_phoenix_server,
+        server(protocol_server_answer),
+        server(invalid_payload_server_answer),
+        server(None, invalid_payload_subscription_server_answer),
+    ],
     indirect=True,
 )
 @pytest.mark.parametrize("query_str", [query1_str])
@@ -131,5 +156,29 @@ async def test_phoenix_channel_protocol_error(event_loop, server, query_str):
 
     query = gql(query_str)
     with pytest.raises(TransportProtocolError):
+        async with Client(transport=sample_transport) as session:
+            await session.execute(query)
+
+
+server_error_subscription_server_answer = (
+    '{"event":"phx_error", "ref":2, "topic":"test_topic"}'
+)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "server", [server(None, server_error_subscription_server_answer)], indirect=True,
+)
+@pytest.mark.parametrize("query_str", [query1_str])
+async def test_phoenix_channel_server_error(event_loop, server, query_str):
+
+    path = "/graphql"
+    url = f"ws://{server.hostname}:{server.port}{path}"
+    sample_transport = PhoenixChannelWebsocketsTransport(
+        channel_name="test_channel", url=url
+    )
+
+    query = gql(query_str)
+    with pytest.raises(TransportServerError):
         async with Client(transport=sample_transport) as session:
             await session.execute(query)
