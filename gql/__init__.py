4bf919c18cc764919d68d8f9964001fc19126363
Hanusz Leszek <leszek.hanusz@gmail.com>
2020-07-12 16:36:23 +0200

    Revert "import_transports_directly_from_gql"
    This reverts commit bfe72a3203f5f242bb13d9bdbae9ef91592536da.
    
    Removing PR #112 from this PR ...

--- gql/__init__.py
+++ gql/__init__.py
@@ -1,13 +1,4 @@
 from .client import Client
 from .gql import gql
-from .transport.aiohttp import AIOHTTPTransport
-from .transport.requests import RequestsHTTPTransport
-from .transport.websockets import WebsocketsTransport
 
-__all__ = [
-    "gql",
-    "AIOHTTPTransport",
-    "Client",
-    "RequestsHTTPTransport",
-    "WebsocketsTransport",
-]
+__all__ = ["gql", "Client"]
