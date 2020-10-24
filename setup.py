e15bf77575a8960d146d92068087db20604edd84
Hanusz Leszek <leszek.hanusz@gmail.com>
2020-10-24 11:25:39 +0200

    Update aiohttp from 3.6.2 to 3.7.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 
 install_requires = [
-    "aiohttp==3.6.2",
+    "aiohttp==3.7.0",
     "graphql-core>=3.1,<3.2",
     "requests>=2.23,<3",
     "websockets>=8.1,<9",
