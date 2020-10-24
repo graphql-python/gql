56d7d7f9a6d8b0994f379b9f6a8f0af85be86c1a
Hanusz Leszek <leszek.hanusz@gmail.com>
2020-10-24 17:49:26 +0200

    Fix typo in docs websocket_async code example

--- docs/code_examples/websockets_async.py
+++ docs/code_examples/websockets_async.py
@@ -11,7 +11,7 @@ async def main():
     # Using `async with` on the client will start a connection on the transport
     # and provide a `session` variable to execute queries on this connection
     async with Client(
-        transport=sample_transport,
+        transport=transport,
         fetch_schema_from_transport=True,
         ) as session:
 
