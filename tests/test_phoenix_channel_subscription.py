4ec38ff05f60eb988d81e135b6884e4fb4a01365
Peter Zingg <peter.zingg@gmail.com>
2021-08-13 09:19:06 -0700

    Add unsubscribe to Phoenix Channel subscription tests

--- tests/test_phoenix_channel_subscription.py
+++ tests/test_phoenix_channel_subscription.py
@@ -9,23 +9,72 @@ from gql import Client, gql
 # Marking all tests in this file with the websockets marker
 pytestmark = pytest.mark.websockets
 
-subscription_server_answer = (
-    '{"event":"phx_reply",'
-    '"payload":'
-    '{"response":'
-    '{"subscriptionId":"test_subscription"},'
-    '"status":"ok"},'
-    '"ref":2,'
-    '"topic":"test_topic"}'
+test_channel = "test_channel"
+test_subscription_id = "test_subscription"
+
+# A server should send this after receiving a 'phx_leave' request message.
+# 'query_id' should be the value of the 'ref' in the 'phx_leave' request. 
+# With only one listener, the transport is closed automatically when
+# it exits a subscription, so this is not used in current tests.
+channel_leave_reply_template = (
+    '{{'
+        '"topic":"{channel_name}",'
+        '"event":"phx_reply",'
+        '"payload":{{'
+            '"response":{{}},'
+            '"status":"ok"'
+        '}},'
+        '"ref":{query_id}'
+    '}}'
 )
 
-countdown_server_answer = (
-    '{{"event":"subscription:data",'
-    '"payload":{{"subscriptionId":"test_subscription","result":'
-    '{{"data":{{"number":{number}}}}}}},'
-    '"ref":{query_id}}}'
+# A server should send this after sending the 'channel_leave_reply'
+# above, to confirm to the client that the channel was actually closed. 
+# With only one listener, the transport is closed automatically when
+# it exits a subscription, so this is not used in current tests.
+channel_close_reply_template = (
+    '{{'
+        '"topic":"{channel_name}",'
+        '"event":"phx_close",'
+        '"payload":{},'
+        '"ref":null'
+    '}}'
 )
 
+# A server sends this when it receives a 'subscribe' request,
+# after creating a unique subscription id. 'query_id' should be the 
+# value of the 'ref' in the 'subscribe' request. 
+subscription_reply_template = (
+    '{{'
+        '"topic":"{channel_name}",'
+        '"event":"phx_reply",'
+        '"payload":{{'
+            '"response":{{'
+                '"subscriptionId":"{subscription_id}"'
+            '}},'
+            '"status":"ok"'
+        '}},'
+        '"ref":{query_id}'
+    '}}'
+)
+
+countdown_data_template = (
+    '{{'
+        '"topic":"{subscription_id}",'
+        '"event":"subscription:data",'
+        '"payload":{{'
+            '"subscriptionId":"{subscription_id}",'
+            '"result":{{'
+                '"data":{{'
+                    '"countdown":{{'
+                        '"number":{number}'
+                    '}}'
+                '}}'
+            '}}'
+        '}},'
+        '"ref":null'
+    '}}'
+)
 
 async def server_countdown(ws, path):
     import websockets
@@ -37,20 +86,26 @@ async def server_countdown(ws, path):
         result = await ws.recv()
         json_result = json.loads(result)
         assert json_result["event"] == "doc"
-        payload = json_result["payload"]
-        query = payload["query"]
+        channel_name = json_result["topic"]
         query_id = json_result["ref"]
 
+        payload = json_result["payload"]
+        query = payload["query"]
         count_found = search("count: {:d}", query)
         count = count_found[0]
         print(f"Countdown started from: {count}")
 
-        await ws.send(subscription_server_answer)
+        await ws.send(subscription_reply_template.format(
+            subscription_id=test_subscription_id,
+            channel_name=channel_name,
+            query_id=query_id))
 
         async def counting_coro():
             for number in range(count, -1, -1):
                 await ws.send(
-                    countdown_server_answer.format(query_id=query_id, number=number)
+                    countdown_data_template.format(
+                        subscription_id=test_subscription_id,
+                        number=number)
                 )
                 await asyncio.sleep(2 * MS)
 
@@ -59,13 +114,19 @@ async def server_countdown(ws, path):
         async def stopping_coro():
             nonlocal counting_task
             while True:
-
                 result = await ws.recv()
                 json_result = json.loads(result)
 
-                if json_result["type"] == "stop" and json_result["id"] == str(query_id):
-                    print("Cancelling counting task now")
-                    counting_task.cancel()
+                if json_result["event"] == "unsubscribe":
+                    query_id = json_result["ref"]
+                    payload = json_result["payload"]
+                    if payload["subscriptionId"] == test_subscription_id:
+                        print("Sending unsubscribe reply")
+                        counting_task.cancel()
+                        await ws.send(subscription_reply_template.format(
+                            subscription_id=test_subscription_id,
+                            channel_name=channel_name,
+                            query_id=query_id))
 
         stopping_task = asyncio.ensure_future(stopping_coro())
 
@@ -81,12 +142,11 @@ async def server_countdown(ws, path):
         except asyncio.CancelledError:
             print("Now stopping task is cancelled")
 
-        await PhoenixChannelServerHelper.send_close(ws)
+        # await PhoenixChannelServerHelper.send_close(ws)
     except websockets.exceptions.ConnectionClosedOK:
-        pass
+        print("Connection closed")
     finally:
         await ws.wait_closed()
-
 
 countdown_subscription_str = """
     subscription {{
@@ -100,19 +160,25 @@ countdown_subscription_str = """
 @pytest.mark.asyncio
 @pytest.mark.parametrize("server", [server_countdown], indirect=True)
 @pytest.mark.parametrize("subscription_str", [countdown_subscription_str])
-async def test_phoenix_channel_subscription(event_loop, server, subscription_str):
+@pytest.mark.parametrize("end_count", [0, 5])
+async def test_phoenix_channel_subscription(event_loop, server, subscription_str, end_count):
+    """Parameterized test. 
+
+    :param end_count: Target count at which the test will 'break' to unsubscribe.
+    """
     import logging
     from gql.transport.phoenix_channel_websockets import (
         PhoenixChannelWebsocketsTransport,
     )
     from gql.transport.websockets import log as websockets_logger
-
+    from gql.transport.phoenix_channel_websockets import log as phoenix_logger
     websockets_logger.setLevel(logging.DEBUG)
+    phoenix_logger.setLevel(logging.DEBUG)
 
     path = "/graphql"
     url = f"ws://{server.hostname}:{server.port}{path}"
     sample_transport = PhoenixChannelWebsocketsTransport(
-        channel_name="test_channel", url=url
+        channel_name=test_channel, url=url
     )
 
     count = 10
@@ -120,40 +186,79 @@ async def test_phoenix_channel_subscription(event_loop, server, subscription_str
 
     async with Client(transport=sample_transport) as session:
         async for result in session.subscribe(subscription):
-
-            number = result["number"]
+            number = result["countdown"]["number"]
             print(f"Number received: {number}")
 
             assert number == count
+            if number == end_count:
+                # Breaking will unsubscribe
+                break
             count -= 1
 
-    assert count == -1
+    assert count == end_count
 
 
-heartbeat_server_answer = (
-    '{{"event":"subscription:data",'
-    '"payload":{{"subscriptionId":"test_subscription","result":'
-    '{{"data":{{"heartbeat_count":{count}}}}}}},'
-    '"ref":1}}'
+heartbeat_data_template = (
+    '{{'
+        '"topic":"{subscription_id}",'
+        '"event":"subscription:data",'
+        '"payload":{{'
+            '"subscriptionId":"{subscription_id}",'
+            '"result":{{'
+                '"data":{{'
+                    '"heartbeat":{{'
+                        '"heartbeat_count":{count}'
+                    '}}'
+                '}}'
+            '}}'
+        '}},'
+        '"ref":null'
+    '}}'
 )
-
 
 async def phoenix_heartbeat_server(ws, path):
     from .conftest import PhoenixChannelServerHelper
 
-    await PhoenixChannelServerHelper.send_connection_ack(ws)
-    await ws.recv()
-    await ws.send(subscription_server_answer)
+    try:
+        await PhoenixChannelServerHelper.send_connection_ack(ws)
 
-    for i in range(3):
-        heartbeat_result = await ws.recv()
-        json_result = json.loads(heartbeat_result)
-        assert json_result["event"] == "heartbeat"
-        await ws.send(heartbeat_server_answer.format(count=i))
+        result = await ws.recv()
+        json_result = json.loads(result)
+        assert json_result["event"] == "doc"
+        channel_name = json_result["topic"]
+        query_id = json_result["ref"]
 
-    await PhoenixChannelServerHelper.send_close(ws)
-    await ws.wait_closed()
+        await ws.send(subscription_reply_template.format(
+            subscription_id=test_subscription_id,
+            channel_name=channel_name,
+            query_id=query_id))
 
+        async def heartbeat_coro():
+            i = 0
+            while True:
+                heartbeat_result = await ws.recv()
+                json_result = json.loads(heartbeat_result)
+                if json_result["event"] == "heartbeat":
+                    await ws.send(heartbeat_data_template.format(
+                        subscription_id=test_subscription_id,
+                        count=i))
+                    i = i + 1
+                elif json_result["event"] == "unsubscribe":
+                    query_id = json_result["ref"]
+                    payload = json_result["payload"]
+                    if payload["subscriptionId"] == test_subscription_id:
+                        print("Sending unsubscribe reply")
+                        await ws.send(subscription_reply_template.format(
+                            subscription_id=test_subscription_id,
+                            channel_name=channel_name,
+                            query_id=query_id))
+
+        await asyncio.wait_for(heartbeat_coro(), 60)
+        # await PhoenixChannelServerHelper.send_close(ws)
+    except websockets.exceptions.ConnectionClosedOK:
+        print("Connection closed")
+    finally:
+        await ws.wait_closed()
 
 heartbeat_subscription_str = """
     subscription {
@@ -175,15 +280,18 @@ async def test_phoenix_channel_heartbeat(event_loop, server, subscription_str):
     path = "/graphql"
     url = f"ws://{server.hostname}:{server.port}{path}"
     sample_transport = PhoenixChannelWebsocketsTransport(
-        channel_name="test_channel", url=url, heartbeat_interval=1
+        channel_name=test_channel, url=url, heartbeat_interval=1
     )
 
     subscription = gql(heartbeat_subscription_str)
     async with Client(transport=sample_transport) as session:
         i = 0
         async for result in session.subscribe(subscription):
-            heartbeat_count = result["heartbeat_count"]
+            heartbeat_count = result["heartbeat"]["heartbeat_count"]
             print(f"Heartbeat count received: {heartbeat_count}")
 
             assert heartbeat_count == i
+            if heartbeat_count == 5:
+                # Breaking will unsubscribe
+                break
             i += 1
