d4c8a7b06d39ae44645994de3e66db5ad3c50b07
Chad Furman <chad@chadfurman.com>
2021-09-12 22:09:51 -0400

    chore: added botocore dependency

--- setup.py
+++ setup.py
@@ -44,8 +44,12 @@ install_websockets_requires = [
     "websockets>=9,<10",
 ]
 
+install_aws_requires = [
+    "botocore>=1.21,<1.22",
+] + install_websockets_requires
+
 install_all_requires = (
-    install_aiohttp_requires + install_requests_requires + install_websockets_requires
+    install_aiohttp_requires + install_requests_requires + install_websockets_requires + install_aws_requires
 )
 
 # Get version from __version__.py file
@@ -87,6 +91,7 @@ setup(
         "aiohttp": install_aiohttp_requires,
         "requests": install_requests_requires,
         "websockets": install_websockets_requires,
+        "aws": install_aws_requires,
     },
     include_package_data=True,
     zip_safe=False,
