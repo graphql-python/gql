adf5a385966abdaa1ec83405a9784c160e8f1a19
Hanusz Leszek <leszek.hanusz@gmail.com>
2021-11-30 22:43:30 +0100

    Add AWS code examples

---
+++ docs/code_examples/aws_api_key_mutation.py
@@ -0,0 +1,53 @@
+import asyncio
+import logging
+import os
+import sys
+from urllib.parse import urlparse
+
+from gql import Client, gql
+from gql.transport.aiohttp import AIOHTTPTransport
+from gql.transport.awsappsyncwebsocket import AppSyncApiKeyAuthorization
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+async def main():
+
+    # Should look like:
+    # https://XXXXXXXXXXXXXXXXXXXXXXXXXX.appsync-api.REGION.amazonaws.com/graphql
+    url = os.environ.get("AWS_GRAPHQL_API_ENDPOINT")
+    api_key = os.environ.get("AWS_GRAPHQL_API_KEY")
+
+    if url is None or api_key is None:
+        print("Missing environment variables")
+        sys.exit()
+
+    # Extract host from url
+    host = str(urlparse(url).netloc)
+
+    auth = AppSyncApiKeyAuthorization(host=host, api_key=api_key)
+
+    transport = AIOHTTPTransport(url=url, headers=auth.get_headers())
+
+    async with Client(
+        transport=transport, fetch_schema_from_transport=False,
+    ) as session:
+
+        query = gql(
+            """
+mutation createMessage($message: String!) {
+  createMessage(input: {message: $message}) {
+    id
+    message
+    createdAt
+  }
+}"""
+        )
+
+        variable_values = {"message": "Hello world!"}
+
+        result = await session.execute(query, variable_values=variable_values)
+        print(result)
+
+
+asyncio.run(main())
---
+++ docs/code_examples/aws_iam_subscription.py
@@ -0,0 +1,42 @@
+import asyncio
+import logging
+import os
+import sys
+
+from gql import Client, gql
+from gql.transport.awsappsyncwebsocket import AppSyncWebsocketsTransport
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+async def main():
+
+    # Should look like:
+    # https://XXXXXXXXXXXXXXXXXXXXXXXXXX.appsync-api.REGION.amazonaws.com/graphql
+    url = os.environ.get("AWS_GRAPHQL_API_ENDPOINT")
+    api_key = os.environ.get("AWS_GRAPHQL_API_KEY")
+
+    if url is None or api_key is None:
+        print("Missing environment variables")
+        sys.exit()
+
+    # Using implicit auth (IAM)
+    transport = AppSyncWebsocketsTransport(url=url)
+
+    async with Client(transport=transport) as session:
+
+        subscription = gql(
+            """
+subscription onCreateMessage {
+  onCreateMessage {
+    message
+  }
+}
+"""
+        )
+
+        async for result in session.subscribe(subscription):
+            print(result)
+
+
+asyncio.run(main())
