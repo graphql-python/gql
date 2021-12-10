09e4b306988942216b3469591da2efb67bea5c2c
Hanusz Leszek <leszek.hanusz@gmail.com>
2021-12-10 08:08:13 +0100

    Add skip and include directive in introspection schema (#279)

--- tests/starwars/test_validation.py
+++ tests/starwars/test_validation.py
@@ -60,7 +60,35 @@ def introspection_schema():
     return Client(introspection=StarWarsIntrospection)
 
 
-@pytest.fixture(params=["local_schema", "typedef_schema", "introspection_schema"])
+@pytest.fixture
+def introspection_schema_empty_directives():
+    introspection = StarWarsIntrospection
+
+    # Simulate an empty dictionary for directives
+    introspection["__schema"]["directives"] = []
+
+    return Client(introspection=introspection)
+
+
+@pytest.fixture
+def introspection_schema_no_directives():
+    introspection = StarWarsIntrospection
+
+    # Simulate no directives key
+    del introspection["__schema"]["directives"]
+
+    return Client(introspection=introspection)
+
+
+@pytest.fixture(
+    params=[
+        "local_schema",
+        "typedef_schema",
+        "introspection_schema",
+        "introspection_schema_empty_directives",
+        "introspection_schema_no_directives",
+    ]
+)
 def client(request):
     return request.getfixturevalue(request.param)
 
@@ -187,3 +215,44 @@ def test_allows_object_fields_in_inline_fragments(client):
         }
     """
     assert not validation_errors(client, query)
+
+
+def test_include_directive(client):
+    query = """
+        query fetchHero($with_friends: Boolean!) {
+          hero {
+            name
+            friends @include(if: $with_friends) {
+                name
+            }
+          }
+        }
+    """
+    assert not validation_errors(client, query)
+
+
+def test_skip_directive(client):
+    query = """
+        query fetchHero($without_friends: Boolean!) {
+          hero {
+            name
+            friends @skip(if: $without_friends) {
+                name
+            }
+          }
+        }
+    """
+    assert not validation_errors(client, query)
+
+
+def test_build_client_schema_invalid_introspection():
+    from gql.utilities import build_client_schema
+
+    with pytest.raises(TypeError) as exc_info:
+        build_client_schema("blah")
+
+    assert (
+        "Invalid or incomplete introspection result. Ensure that you are passing the "
+        "'data' attribute of an introspection response and no 'errors' were returned "
+        "alongside: 'blah'."
+    ) in str(exc_info.value)
