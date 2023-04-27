db6d277219d174be22a342cb17a0e79bc7cc6975
Hanusz Leszek <leszek.hanusz@gmail.com>
2023-04-27 13:02:47 +0200

    Python 3.11 support (#410)

--- setup.py
+++ setup.py
@@ -89,6 +89,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: Implementation :: PyPy",
     ],
     keywords="api graphql protocol rest relay gql client",
