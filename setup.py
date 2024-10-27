79dbe457bdf6a2c915e2cbfa182dc88625bc405b
Hanusz Leszek <leszek.hanusz@gmail.com>
2024-10-27 14:39:21 +0100

    Adding MIT License classifier (#498)

--- setup.py
+++ setup.py
@@ -84,6 +84,7 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.8",
