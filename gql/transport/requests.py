63a2de7766cbce7c088c3952525f8ef210ae2512
Leszek Hanusz <leszek.hanusz@gmail.com>
2025-05-25 15:31:04 +0200

    Using get_batch_execution_result_list in requests transport

--- gql/transport/requests.py
+++ gql/transport/requests.py
@@ -25,6 +25,7 @@ from requests_toolbelt.multipart.encoder import MultipartEncoder
 from gql.transport import Transport
 
 from ..graphql_request import GraphQLRequest
+from .common.batch import get_batch_execution_result_list
 from .exceptions import (
     TransportAlreadyConnected,
     TransportClosed,
@@ -340,52 +341,7 @@ class RequestsHTTPTransport(Transport):
 
         answers = self._extract_response(response)
 
-        self._validate_answer_is_a_list(answers)
-        self._validate_num_of_answers_same_as_requests(reqs, answers)
-        self._validate_every_answer_is_a_dict(answers)
-        self._validate_data_and_errors_keys_in_answers(answers)
-
-        return [self._answer_to_execution_result(answer) for answer in answers]
-
-    def _answer_to_execution_result(self, result: Dict[str, Any]) -> ExecutionResult:
-        return ExecutionResult(
-            errors=result.get("errors"),
-            data=result.get("data"),
-            extensions=result.get("extensions"),
-        )
-
-    def _validate_answer_is_a_list(self, results: Any) -> None:
-        if not isinstance(results, list):
-            self._raise_invalid_result(
-                str(results),
-                "Answer is not a list",
-            )
-
-    def _validate_data_and_errors_keys_in_answers(
-        self, results: List[Dict[str, Any]]
-    ) -> None:
-        for result in results:
-            if "errors" not in result and "data" not in result:
-                self._raise_invalid_result(
-                    str(results),
-                    'No "data" or "errors" keys in answer',
-                )
-
-    def _validate_every_answer_is_a_dict(self, results: List[Dict[str, Any]]) -> None:
-        for result in results:
-            if not isinstance(result, dict):
-                self._raise_invalid_result(str(results), "Not every answer is dict")
-
-    def _validate_num_of_answers_same_as_requests(
-        self,
-        reqs: List[GraphQLRequest],
-        results: List[Dict[str, Any]],
-    ) -> None:
-        if len(reqs) != len(results):
-            self._raise_invalid_result(
-                str(results),
-                "Invalid answer length",
-            )
+        return get_batch_execution_result_list(reqs, answers)
 
     def _raise_invalid_result(self, result_text: str, reason: str) -> None:
         raise TransportProtocolError(
