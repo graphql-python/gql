2bb8eb9804630fcf002bcb51c78249040626f9a3
Leszek Hanusz <leszek.hanusz@gmail.com>
2025-05-27 20:59:48 +0200

    Remove MIT license classifier

--- setup.py
+++ setup.py
@@ -86,7 +86,6 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries",
-        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.9",
