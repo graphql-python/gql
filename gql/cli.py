df84120a9a1d83ba743d2330bb5bb5e4d0c0df22
Leszek Hanusz <leszek.hanusz@gmail.com>
2025-09-13 21:50:11 +0200

    Add option in docs

--- gql/cli.py
+++ gql/cli.py
@@ -140,7 +140,9 @@ def get_parser(with_examples: bool = False) -> ArgumentParser:
              - input_value_deprecation:false  to omit deprecated input fields
              - specified_by_url:true
              - schema_description:true
-             - directive_is_repeatable:true"""
+             - directive_is_repeatable:true
+             - input_object_one_of:true
+             """
         ),
         dest="schema_download",
     )
